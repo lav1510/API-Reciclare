1db72c26254256f67a9dd96b0f65c6a2fca69952
lav1510 <maria..bugarin@student.upt.ro>
2022-04-28 19:27:23 +0300

    added unit test and mokaroo schemas

--- Reciclare/Models/Produs.cs
+++ Reciclare/Models/Produs.cs
@@ -4,5 +4,7 @@
     {
         public string Nume { get; set; }
         public int Id { get; set; }
+        public int Cantitate { get; set; }
+        public bool Produs_pubele { get; set; }
     }
 }
---
+++ Reciclare/ReciclareApi.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TransportApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
